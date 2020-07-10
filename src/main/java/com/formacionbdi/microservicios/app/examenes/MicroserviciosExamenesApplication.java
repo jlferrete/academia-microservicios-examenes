80cb9bad740fb0950e7f88b423c33020b9bffe5e
JlFerrete <joseluisferrete@gmail.com>
2020-07-10 11:06:36 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/MicroserviciosExamenesApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.app.examenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosExamenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosExamenesApplication.class, args);
+	}
+
+}
