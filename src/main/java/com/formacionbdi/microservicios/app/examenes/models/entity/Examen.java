00055049fc1bdecae73f40ed7caca71c418d080f
JlFerrete <joseluisferrete@gmail.com>
2020-07-10 11:25:59 +0200

    Creating Microservice Tests

---
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/models/entity/Examen.java
@@ -0,0 +1,60 @@
+package com.formacionbdi.microservicios.app.examenes.models.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="examenes")
+public class Examen {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="create_at")
+	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		this.createAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	
+	
+}
