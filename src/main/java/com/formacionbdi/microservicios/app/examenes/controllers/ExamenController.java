22c0fecd9cfdb9e0fad22a0f7919f644fb3029ee
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 09:16:31 +0200

    Search for exams using operator like

--- src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,6 +35,12 @@ public class ExamenController extends CommonController<Examen, ExamenService> {
 		examenDb.setPreguntas(examen.getPreguntas());
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(examenDb));
+	}
+	
+	@GetMapping("/filtrar/{term}")
+	public ResponseEntity<?> filtar(@PathVariable String term) {
+		
+		return ResponseEntity.ok(service.findByNombre(term));
 	}
 
 }
--- src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/ExamenRepository.java
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/ExamenRepository.java
@@ -1,11 +1,15 @@
 package com.formacionbdi.microservicios.app.examenes.models.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 
 public interface ExamenRepository extends CrudRepository<Examen, Long> {
 
-	
+	@Query("select e from Examen e where e.nombre like %?1%")
+	public List<Examen> findByNombre(String term);
 	
 }
