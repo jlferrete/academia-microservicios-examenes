4870784d1eb6995fafb5b4bee039a29abbd492d4
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 13:20:07 +0200

    Adding pagination

--- src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/AsignaturaRepository.java
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/AsignaturaRepository.java
@@ -1,9 +1,10 @@
 package com.formacionbdi.microservicios.app.examenes.models.repository;
 
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Asignatura;
 
-public interface AsignaturaRepository extends CrudRepository<Asignatura, Long> {
+public interface AsignaturaRepository extends PagingAndSortingRepository<Asignatura, Long> {
 
 }
