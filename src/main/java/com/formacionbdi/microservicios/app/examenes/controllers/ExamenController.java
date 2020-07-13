bcbca01e181f50846a1a56517b03c3d56f18dd37
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 07:30:05 +0200

    Adding a handler put method to edit the questions

--- src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
+++ src/main/java/com/formacionbdi/microservicios/app/examenes/controllers/ExamenController.java
@@ -1,5 +1,8 @@
 package com.formacionbdi.microservicios.app.examenes.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.examenes.models.entity.Examen;
+
 import com.formacionbdi.microservicios.app.examenes.services.ExamenService;
 import com.formacionbdi.microservicios.commons.controllers.CommonController;
 
@@ -15,8 +19,22 @@ public class ExamenController extends CommonController<Examen, ExamenService> {
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@RequestBody Examen examen, @PathVariable Long id) {
-		
-		return null;
+
+		Optional<Examen> o = service.findById(id);
+
+		if (!o.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Examen examenDb = o.get();
+		examenDb.setNombre(examen.getNombre());
+
+		examenDb.getPreguntas().stream().filter(pdb -> !examen.getPreguntas().contains(pdb))
+				.forEach(examenDb::removePregunta);
+
+		examenDb.setPreguntas(examen.getPreguntas());
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(examenDb));
 	}
-	
+
 }
